0a77dea6953aebade1fe7309e74fe4ef7ec26544
spoutOG <orange.tiger617@gmail.com>
2025-04-15 20:18:31 -0400

    Upgrade

---
+++ Games/Slope 3_files/styles-thumbnails.php
@@ -0,0 +1,39 @@
+.yarpp-thumbnails-horizontal .yarpp-thumbnail, .yarpp-thumbnail-default, .yarpp-thumbnail-title {
+	display: inline-block;
+	*display: inline;
+}
+.yarpp-thumbnails-horizontal .yarpp-thumbnail {
+	border: 0px solid rgba(127,127,127,0.1);
+	width: 160px;
+	height: 108px;
+	margin: 0px;
+	margin-left: 0px;
+	vertical-align: top;
+}
+.yarpp-thumbnail > img, .yarpp-thumbnail-default {
+	width: 160px;
+	height: 74px;
+	margin: 0px;
+}
+.yarpp-thumbnails-horizontal .yarpp-thumbnail > img, .yarpp-thumbnails-horizontal .yarpp-thumbnail-default {
+	display: block;
+}
+.yarpp-thumbnails-horizontal .yarpp-thumbnail-title {
+	font-size: 1em;
+	max-height: 2.8em;
+	line-height: 1.3em;
+	margin: 0px;
+	margin-top: 0px;
+	width: 160px;
+	text-decoration: inherit;
+	overflow: hidden;
+	text-transform: uppercase;
+}
+
+.yarpp-thumbnail-default {
+	overflow: hidden;
+}
+.yarpp-thumbnail-default > img {
+	min-height: 74px;
+	min-width: 160px;
+}
